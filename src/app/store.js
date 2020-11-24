787620604dab9f7d6206c71dcbea4c5b8d88d1df
Maksym Honchar <maksim.honchar@gmail.com>
2020-11-24 20:23:33 +0200

    add localeStorage to redux state

--- src/app/store.js
+++ src/app/store.js
@@ -3,10 +3,31 @@ import topTracksReducer from '../features/homepage/topTracksSlice'
 import artistReducer from '../features/artistpage/artistPageSlice'
 import findTrackReducer from '../features/search_track_page/searchTrackSlice'
 
-export default configureStore({
-  reducer: {
-    top_tracks: topTracksReducer,
-    performer: artistReducer,
-    find_track: findTrackReducer
-  }
+import { combineReducers } from '@reduxjs/toolkit'
+import { loadState, saveState } from './localeStorage'
+import throttle from 'lodash.throttle'
+
+
+const rootReducer = combineReducers({
+  top_tracks: topTracksReducer,
+  performer: artistReducer,
+  find_track: findTrackReducer
 })
+
+const persistedState = loadState()
+
+const store = configureStore({
+  reducer: rootReducer,
+  persistedState
+})
+
+store.subscribe(throttle(() => {
+  saveState({
+    top_tracks: store.getState().top_tracks,
+    performer: store.getState().performer,
+    find_track: store.getState().performer
+  })
+}, 1000))
+
+
+export default store
