b25869c6e41ff97cc2d0edf6c2a221f6d54d1483
Maksym Honchar <maksim.honchar@gmail.com>
2020-11-25 10:16:08 +0200

    remove rootreducer from store

--- src/app/store.js
+++ src/app/store.js
@@ -3,21 +3,17 @@ import topTracksReducer from '../features/homepage/topTracksSlice'
 import artistReducer from '../features/artistpage/artistPageSlice'
 import findTrackReducer from '../features/search_track_page/searchTrackSlice'
 
-import { combineReducers } from '@reduxjs/toolkit'
 import { loadState, saveState } from './localeStorage'
 import throttle from 'lodash.throttle'
-
-
-const rootReducer = combineReducers({
-  top_tracks: topTracksReducer,
-  performer: artistReducer,
-  find_track: findTrackReducer
-})
 
 const persistedState = loadState()
 
 const store = configureStore({
-  reducer: rootReducer,
+  reducer: {
+    top_tracks: topTracksReducer,
+    performer: artistReducer,
+    find_track: findTrackReducer
+  },
   persistedState
 })
 
